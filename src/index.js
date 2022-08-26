eb3d166c6c92cd4fdbea924b7d5c9827e4c2c3c4
ViktoriyaZinyak <viktoriazinyak@gmail.com>
2022-08-26 11:54:47 +0300

    debounce

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
+const input = document.querySelector('#search-box');
 const DEBOUNCE_DELAY = 300;
+
+input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(e) {
+  fetchCountries(e.target.value);
+}
+
+function fetchCountries(name) {
+  console.log(
+    fetch(
+      `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flag,languages`
+    ).then(response => response.json())
+  );
+}
+
+function renderTamplate() {
+  `<li>
+    <svg width="90" height="70">
+      <use href="#"></use>
+    </svg>
+  </li>`;
+}
+
+// fetchCountries('peru');
